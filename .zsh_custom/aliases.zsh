3d44f9e0e5c90254d50b21944fd47b72f43aa38e
Jesse Rushlow <jr@rushlow.dev>
2024-02-29 00:49:12 -0500

    a few more updates

--- .zsh_custom/aliases.zsh
+++ .zsh_custom/aliases.zsh
@@ -10,3 +10,8 @@ function move {
 
 # makes commands like "git log" and "grep" not clear the screen after exit
 PAGER='less -X'
+
+#alias workspace="docker-compose run --rm workspace bash"
+#alias dcs="docker-compose start php-fpm nginx database"
+alias cdp="cd ~/develop/php"
+#alias php-fixer="docker-compose run --rm php-cli tools/php-cs-fixer/vendor/bin/php-cs-fixer fix"
